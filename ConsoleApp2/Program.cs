0a399993e99a4384e71c717695e4d5731c928a81
Nada Zidan <nadazidan75@outlook.com>
2023-04-13 14:51:06 +0300

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+void GetFloorNo (int numberOfFloors, int bulbWeight, int i=1, int numberOfRestries =1)
+{
+
+    if (bulbWeight == i)
+    {
+        Console.WriteLine("floor no: "+i);
+        Console.WriteLine("number of retires: " + (numberOfRestries ));
+        return;
+    }
+    else
+    {
+        if (bulbWeight > i)
+        {
+            if (numberOfFloors - i == 1)
+                i++;
+            else
+                i += (numberOfFloors - i) / 2;
+        }
+        else if (bulbWeight < i)
+        {
+            numberOfFloors = i;
+            i = (i - 1) / 2;
+        }
+        numberOfRestries++;
+        GetFloorNo(numberOfFloors, bulbWeight, i, numberOfRestries);
+    }
+}
+
+
+
+GetFloorNo(2000000, 2000000);
+
